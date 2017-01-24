9c084da06369bc855ee796ee8700ba4ed3493d71
Nathan Landis <nathan.landis@rallyhealth.com>
2017-01-24 17:43:07 -0600

    Initialize Core projects

---
+++ core/src/Avalanche/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Avalanche
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class AvalancheService
+    {
+
+    }
+}
---
+++ core/test/Avalanche.Tests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace Avalanche.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1() 
+        {
+            var obj = new AvalancheService();
+            Assert.True(obj != null);
+        }
+    }
+}
