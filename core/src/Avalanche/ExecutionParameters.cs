21093cf43fd6e7aa59049961455dff13ca43571b
Nathan Landis <nathan.landis@rallyhealth.com>
2017-01-24 18:47:56 -0600

    Move over ExecutionParameters, setup DI and logging

---
+++ core/src/Avalanche/ExecutionParameters.cs
@@ -0,0 +1,127 @@
+using Amazon;
+using Mono.Options;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Avalanche
+{
+    public class ExecutionParameters
+    {
+        public GlacierParameters Glacier { get; set; }
+        public AvalancheParameters Avalanche { get; set; }
+
+        public IEnumerable<string> GetValidationErrors()
+        {
+            return Glacier.GetValidationErrors().Union(Avalanche.GetValidationErrors());
+        }
+    }
+
+    public static class ExecutionParameterHelpers
+    {
+        public static string ResolveConfigFileLocation(string[] args)
+        {
+            var showHelp = false;
+            string configFileLocation = null;
+            var options = new OptionSet
+            {
+                { "c=|config-file=", "REQUIRED: Path/File for Avalanche Config File", a => configFileLocation = a.Trim('"') },
+                { "h|help", "Help", a => showHelp = a != null }
+            };
+
+            if (showHelp)
+            {
+                options.WriteOptionDescriptions(Console.Out);
+                return null;
+            }
+            else if(string.IsNullOrEmpty(configFileLocation)
+                    || !File.Exists(configFileLocation))
+            {
+                Console.Error.WriteLine("Need to provide a valid config file location");
+            }
+
+            return configFileLocation;
+        }
+
+        public static ExecutionParameters LoadExecutionParameters(string configFileLocation)
+        {
+            var serialized = File.ReadAllText(configFileLocation);
+            return JsonConvert.DeserializeObject<ExecutionParameters>(serialized);
+        }
+    }
+
+    public class GlacierParameters
+    {
+        public string AccountId { get; set; }
+        public string AccessKeyId { get; set; }
+        public string SecretAccessKey { get; set; }
+
+        public string SnsTopicId { get; set; }
+
+        public string Region { get; set; }
+        public string VaultName { get; set; }
+
+        public RegionEndpoint GetRegion()
+        {
+            foreach (var r in RegionEndpoint.EnumerableAllRegions)
+            {
+                if (r.SystemName.Equals(Region, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    return r;
+                }
+            }
+            return null;
+        }
+
+        public IEnumerable<string> GetValidationErrors()
+        {
+            if (string.IsNullOrWhiteSpace(AccessKeyId))
+            {
+                yield return "Glacier Access Key ID is required.";
+            }
+            if (string.IsNullOrWhiteSpace(SecretAccessKey))
+            {
+                yield return "Glacier Secret Access Key is required.";
+            }
+
+            if (string.IsNullOrWhiteSpace(Region))
+            {
+                yield return "Glacier Region is required.";
+            }
+            else if (GetRegion() == null)
+            {
+                yield return "Could not parse given Glacier Region " + GetRegion();
+            }
+
+            if (string.IsNullOrWhiteSpace(VaultName))
+            {
+                yield return "Glacier Vault Name is required.";
+            }
+        }
+    }
+
+    public class AvalancheParameters
+    {
+        public string CatalongFilePath { get; set; }
+        public string AvalancheFilePath { get; set; }
+
+        public IEnumerable<string> GetValidationErrors()
+        {
+            if (string.IsNullOrWhiteSpace(CatalongFilePath))
+            {
+                yield return "Lightroom Catalog file path is required";
+            }
+            else if (!File.Exists(CatalongFilePath))
+            {
+                yield return "Could not find a Lightroom Catalog file at path " + CatalongFilePath;
+            }
+
+            if (string.IsNullOrWhiteSpace(AvalancheFilePath))
+            {
+                yield return "Avalanch File path is required";
+            }
+        }
+    }
+}
--- core/src/Avalanche/Program.cs
+++ core/src/Avalanche/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Logging;
 
 namespace Avalanche
 {
@@ -6,12 +8,30 @@ namespace Avalanche
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var container = new ServiceCollection().AddLogging();
+            container.AddSingleton<AvalancheRunner>();
+
+            var serviceProvider = container.BuildServiceProvider();
+            var logFactory = serviceProvider.GetService<ILoggerFactory>();
+            logFactory.AddConsole();
+            
+            var runner = serviceProvider.GetService<AvalancheRunner>();
+            runner.Run();
         }
     }
 
-    public class AvalancheService
+    public class AvalancheRunner
     {
+        private readonly ILogger<AvalancheRunner> _logger;
+        
+        public AvalancheRunner(ILogger<AvalancheRunner> logger)
+        {
+            _logger = logger;
+        }
 
+        public void Run()
+        {
+            _logger.LogInformation("Hello World!");
+        }
     }
 }
