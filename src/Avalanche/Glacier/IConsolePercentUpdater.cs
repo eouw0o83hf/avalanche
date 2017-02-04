73b41c83503778a0e5eb62f1b6db184858deaa87
Nathan Landis <nathanlandis@gmail.com>
2017-02-04 16:37:44 -0600

    Remove old Windows-only app, core only now

--- src/Avalanche/Glacier/IConsolePercentUpdater.cs
+++ src/Avalanche/Glacier/IConsolePercentUpdater.cs
@@ -13,10 +13,22 @@ namespace Avalanche.Glacier
     // else is wired together and mostly working.
     public class ConsolePercentUpdater : IConsolePercentUpdater
     {
+        // Making some tradeoffs to this so I can run it and get up to date.
+        // Long-term, it needs to handle multi-threaded updating
+        private int _lastUpdateAmount = -1;
+        
         // todo: make this not stupid
         public void UpdatePercentage(string file, int percent)
         {
+            // Only update every 10% to curb the spam
+            percent = (percent / 10) * 10;
+            if(percent == _lastUpdateAmount)
+            {
+                return;
+            }
+           
             Console.WriteLine($"{file} is at {percent}%");
+            _lastUpdateAmount = percent;
         }
     }
 }
