29b93e4ad4b9e9311f5588e6b9e05846085866d4
Nathan Landis <nathanlandis@gmail.com>
2017-02-16 11:32:29 -0600

    Add parallelism in AvalancheRunner

--- src/Avalanche/Runner/IAvalancheRunner.cs
+++ src/Avalanche/Runner/IAvalancheRunner.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -18,6 +20,7 @@ namespace Avalanche.Runner
     public class AvalancheRunner : IAvalancheRunner
     {
         private const int RetryCount = 3;
+        private const int UploadParallelism = 3;
         
         private readonly ILogger<AvalancheRunner> _logger;
         private readonly IGlacierGateway _glacier;
@@ -25,6 +28,18 @@ namespace Avalanche.Runner
         private readonly IAvalancheRepository _avalanche;
         private readonly ExecutionParameters _parameters;
         
+        private class RunState
+        {
+            public ConcurrentQueue<PictureModel> WorkQueue { get; set; }
+            
+            public int Index { get; set; }
+            public int FilteredPictureCount { get; set; }
+
+            public Guid CatalogId { get; set; }
+
+            public AvalancheRunResult Result { get; set; }
+        }
+
         // todo: convert some of these to factories so we can safely parallelize
         //       also make sure we don't cause weird interlocking issues on sqlite dbs
         public AvalancheRunner(ILogger<AvalancheRunner> logger, IGlacierGateway glacier,
@@ -40,57 +55,95 @@ namespace Avalanche.Runner
 
         public async Task<AvalancheRunResult> Run()
         {
-            var result = new AvalancheRunResult();
-            
-            var catalogId = _lightroom.GetCatalogId();
-            var allPictures = _lightroom.GetAllPictures();
-            var filteredPictures = allPictures
+            // This needs to be created or else all of the upload attempts will fail.
+            // It's the container that the archives live in.                                   
+            await _glacier.AssertVaultExists(_parameters.Glacier.VaultName);
+                        
+            var filteredPictures = _lightroom.GetAllPictures()
+                                    // A Picture can be in multiple Catalogs, but
+                                    // only needs to be backed up a single time
                                     .GroupBy(a => a.FileId)
                                     .Select(a => a.First())
                                     .Where(a => a.LibraryCount > 0 
-                                                && !_avalanche.FileIsArchived(a.FileId))
-                                    .ToList();
+                                                // Make sure we don't double archive
+                                                && !_avalanche.FileIsArchived(a.FileId));
 
-            _logger.LogInformation("Backing up {0} images", filteredPictures.Count);
-            
-            await _glacier.AssertVaultExists(_parameters.Glacier.VaultName);
-            
-            // todo: parallelize this if it improves uploads (not sure if it will)
-            var index = 0;
-            foreach(var f in filteredPictures)
+            // In order to support upload parallelism, we're going to place
+            // all of the work to be done in a Queue, then launch workers
+            // which will pull from it. There's probably a more sophisticated
+            // way to do this with stringing Tasks together, but the complexity
+            // and number of edge cases are pretty intense. Having some longer-
+            // running Tasks is way more foolproof.
+            var workQueue = new ConcurrentQueue<PictureModel>(filteredPictures);
+            _logger.LogInformation("Backing up {0} images", workQueue.Count);
+
+            var runState = new RunState
             {
-                var currentPath = Path.Combine(f.AbsolutePath, f.FileName);
-                _logger.LogInformation("Archiving {0} of {1}: {2}", ++index, filteredPictures.Count, currentPath);
+                Index = 0,
+                FilteredPictureCount = workQueue.Count,
+                CatalogId = _lightroom.GetCatalogId(),
+                Result = new AvalancheRunResult(),
+                WorkQueue = workQueue
+            };
 
-                // Retry for transient transport failures
-                ArchivedPictureModel archive = null;
-                for(var i = 0; i < RetryCount; ++i)
-                {
-                    try
-                    {
-                        archive = await _glacier.SaveImage(f, _parameters.Glacier.VaultName);
-                        break;
-                    }
-                    catch(Exception ex)
-                    {
-                        _logger.LogError("Error persisting file", ex);
-                        continue;
-                    }
-                }
-
-                if(archive == null)
-                {
-                    _logger.LogError("Failed 3 times to persist {0}, giving up", currentPath);
-                    result.Failures.Add(f);
-                    continue;
-                }
-
-                _avalanche.MarkFileAsArchived(archive, _parameters.Glacier.VaultName, _parameters.Glacier.Region, _parameters.Avalanche.CatalogFilePath, catalogId.ToString());
-                result.Successes.Add(f);
-            }
+            // Start up some long-running Tasks which will pull the
+            // archive work from the Queue
+            var workers = Enumerable.Range(0, UploadParallelism)
+                                    .Select(a => ArchiveWorker(runState));
+            await Task.WhenAll(workers);
 
             _logger.LogInformation("Done");
-            return result;
+            return runState.Result;
         }
+
+        private async Task ArchiveWorker(RunState state)
+        {
+            // If this were Scala we could just make this a tail-recursive method.
+            // But it's not. So here's that one time where it would be nice.
+            while(state.WorkQueue.Any())
+            {
+                PictureModel picture;
+                if(!state.WorkQueue.TryDequeue(out picture))
+                {
+                    return;
+                }
+
+                await ArchivePicture(picture, state);
+            }
+        }
+
+        private async Task ArchivePicture(PictureModel picture, RunState state)
+        {
+            var currentPath = Path.Combine(picture.AbsolutePath, picture.FileName);
+            _logger.LogInformation("Archiving {0} of {1}: {2}", ++state.Index, state.FilteredPictureCount, currentPath);
+
+
+            // Retry for transient transport failures
+            ArchivedPictureModel archive = null;
+            for(var i = 0; i < RetryCount; ++i)
+            {
+                try
+                {
+                    archive = await _glacier.SaveImage(picture, _parameters.Glacier.VaultName);
+                    break;
+                }
+                catch(Exception ex)
+                {
+                    _logger.LogError("Error persisting file", ex);
+                    continue;
+                }
+            }
+
+            if(archive == null)
+            {
+                _logger.LogError("Failed 3 times to persist {0}, giving up", currentPath);
+                state.Result.Failures.Add(picture);
+                return;
+            }
+
+            _avalanche.MarkFileAsArchived(archive, _parameters.Glacier.VaultName, _parameters.Glacier.Region, _parameters.Avalanche.CatalogFilePath, state.CatalogId.ToString());
+            state.Result.Successes.Add(picture);
+        }
+
     }
 }
